a6560db62ef4125d3ef7f35cb69cf2a98bfae2ac
Peter Saba <petersaba001@gmail.com>
2022-10-15 00:07:51 +0300

    backend <> database: importing env and config

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+require('dotenv').config();
+require('./config/config')
+
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT_NUMBER, (err) => {
     if(err) console.log(err)
 
-    console.log('Server is running');
+    console.log(`Server is running ${process.env.PORT_NUMBER}`);
 })
