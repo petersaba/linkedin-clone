45b459924bfb685dc2c65680ec5ebdfbaf0c57bb
Peter Saba <petersaba001@gmail.com>
2022-10-16 21:42:01 +0300

    backend <> jobs: adding new in company job posts

--- backend/controllers/jobs.controller.js
+++ backend/controllers/jobs.controller.js
@@ -3,7 +3,20 @@ const { User } = require('../models/users.model');
 
 module.exports.createJob = async (req, res) => {
     try{
-        
+        const params = req.fields;
+
+        const job = await Job.create({ ...params, company_id: req.user.id});
+
+        await User.findByIdAndUpdate(req.user.id,
+            {
+                $push: { job_posts: job.id }
+            });
+
+        res.status(200).json({
+            status: 'Success',
+            message: job
+        });
+
     }catch(err){
         console.log('Error from database');
         res.status(400).json({
