9bd2ccfd3731dc839a3533ce9234b2f6874a1f1b
Peter Saba <petersaba001@gmail.com>
2022-10-16 13:17:44 +0300

    backend <> signup: moving email check to auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -70,3 +70,14 @@ module.exports.login = async (req, res) => {
         message: invalid_message
     });
 }
+
+module.exports.isEmailAlreadyUsed = async (req, res) => {
+    const user = await User.findOne({email: req.query.email});
+
+    if(user){
+        return res.status(409).json({ status: "Error",
+                                        message: "Email is already used" });
+    }
+
+    res.status(200).json({ status: "Success" });
+}
--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,12 +1,1 @@
 const { User } = require('../models/users.model');
-
-module.exports.isEmailAlreadyUsed = async (req, res) => {
-    const user = await User.findOne({email: req.query.email});
-
-    if(user){
-        return res.status(409).json({ status: "Error",
-                                        message: "Email is already used" });
-    }
-
-    res.status(200).json({ status: "Success" });
-}
