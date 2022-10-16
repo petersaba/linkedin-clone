2dc8b4cc4cdcec1384a7c7c5813504e968cd2429
Peter Saba <petersaba001@gmail.com>
2022-10-16 18:21:30 +0300

    backend <> general: saving images in images folder and saving apth in db

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,9 +1,31 @@
 const { User } = require('../models/users.model');
+const { base64ToImage } = require('../utilities/utilities');
 
 module.exports.editProfile = async (req, res) => {
     const params = req.fields;
-    console.log(params);
+    if(params.profile_url){
+        delete params.profile_url;
+    }
+    if(params.background_url){
+        delete params.background_url;
+    }
+    
+    let unique_date = new Date();
+    unique_date = JSON.stringify(unique_date).replace(/"/g, '');
+    if(params.profile_image){
+        const image_name = `profile${req.user.id}${unique_date}`;
+        params.profile_url = base64ToImage(params.profile_image, image_name);
+    }
+    if(params.background_image){
+        const image_name = `background${req.user.id}`;
+        params.background_url = base64ToImage(params.background_image, image_name);
+    }
+
     const user = await User.findByIdAndUpdate(req.user.id, { ...params });
 
-    res.status(204).json({status: 'Success'});
+    console.log(user);
+    res.status(200).json({
+        status: 'Success',
+        message: user
+    });
 }
