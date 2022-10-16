2a6d1fbc0531b5425004068254cd4d337418b6e3
Peter Saba <petersaba001@gmail.com>
2022-10-16 20:27:26 +0300

    backend <> models: adding job model

---
+++ backend/models/jobs.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const jobsSchema = mongoose.Schema({
+    company_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: 'Company id is required',
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        required: 'Title is required'
+    },
+    type: {
+        type: String,
+        required: 'Type is required',
+        enum: ['On-site', 'Remote']
+    },
+    time: {
+        type: String,
+        required: 'Time is required',
+        enum: ['Full-time', 'Part-time']
+    },
+    level: {
+        type: String,
+        required: 'Level is required',
+        enum: ['Entry-level', 'Mid-level', 'Senior-level']
+    },
+    date_created: {
+        type: Date,
+        default: Date.now
+    },
+    about: {
+        type: String,
+        required: 'About is required'
+    },
+    applicants: {
+        type: Array
+    }
+});
+
+module.exports.Job = mongoose.model('Job', jobsSchema);
