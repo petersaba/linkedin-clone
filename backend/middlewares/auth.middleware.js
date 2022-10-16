b84bea32715e715f978e4bec6f4b2498c1c3d843
Peter Saba <petersaba001@gmail.com>
2022-10-16 13:47:39 +0300

    backend <> middlewares: checking for token in request

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -9,5 +9,13 @@ module.exports.authMiddleware = async (req, res, next) => {
         });
     }
 
+    const token = req.headers.authorization.split(' ')[1];
+
+    if(!token){
+        return res.status(401).json({
+            status: 'Error',
+            message: 'Unauthorized'
+        });
+    }
     next();
 }
