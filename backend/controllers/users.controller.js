1ccd74569e171f3d0ad37a21689949944b5f94fe
Peter Saba <petersaba001@gmail.com>
2022-10-16 15:43:30 +0300

    backend <> edit profile: updating everything except profile and background in database

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,1 +1,9 @@
 const { User } = require('../models/users.model');
+
+module.exports.editProfile = async (req, res) => {
+    const params = req.fields;
+    console.log(params);
+    const user = await User.findByIdAndUpdate(req.user.id, { ...params });
+
+    res.status(204).json({status: 'Success'});
+}
