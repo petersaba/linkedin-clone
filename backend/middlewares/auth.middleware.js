4302353e6582f99273bb98f0a4bdea25229b4b7f
Peter Saba <petersaba001@gmail.com>
2022-10-16 14:04:58 +0300

    backend <> middlewares: verifying if token is valid

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { User } = require('../models/users.model');
 
 module.exports.authMiddleware = async (req, res, next) => {
-    if(!req.authorization){
+    if(!req.headers.authorization){
         return res.status(401).json({
             status: 'Error',
             message: 'Unauthorized'
@@ -17,5 +17,16 @@ module.exports.authMiddleware = async (req, res, next) => {
             message: 'Unauthorized'
         });
     }
-    next();
+
+    try{
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        const user = await User.findOne({ id: decoded.id });
+        req.user = user;
+        next();
+    }catch(err){
+        res.status(401).json({
+            status: 'Error',
+            message: 'Unauthorized'
+        });
+    }
 }
