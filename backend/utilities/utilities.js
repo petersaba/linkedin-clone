ff40a876c3ed6123ee04ac17404488f7d57373a9
Peter Saba <petersaba001@gmail.com>
2022-10-16 18:11:07 +0300

    backend <> utilities: adding image to images folder

--- backend/utilities/utilities.js
+++ backend/utilities/utilities.js
@@ -1,6 +1,10 @@
-module.exports.base64ToImage = (base64_image, file_path) => {
+const fs = require('fs');
+
+module.exports.base64ToImage = (base64_image, file_name) => {
     base64_str = base64_image.split(',')[1];
 
     const buffer = new Buffer.from(base64_str, 'base64');
-    fs.writeFileSync(file_path, buffer);
+    fs.writeFileSync(`./images/${file_name}.jpg`, buffer);
+
+    return file_name;
 }
