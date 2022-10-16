c85100f23bd91f7d9da6a35499655864e54b345a
Peter Saba <petersaba001@gmail.com>
2022-10-17 02:44:24 +0300

    backend <> unfollow company: adding necessary validations

--- backend/controllers/actions.controller.js
+++ backend/controllers/actions.controller.js
@@ -45,3 +45,27 @@ module.exports.followCompany = async (req, res) => {
         message: 'Companhy followed successfully'
     });
 }
+
+modules.exports.unfollowCompany = async (req, res) => {
+    if(req.user.user_type != 'worker'){
+        return res.status(400).json({
+            status: 'Error',
+            message: 'User is not a worker'
+        });
+    }
+
+    if(!req.fields.id){
+        return res.status(400).json({
+            status: 'Error',
+            message: 'Company id is required'
+        });
+    }
+
+    const company = await User.findOne({ id: req.fields.id, user_type: 'company' });
+    if(!company){
+        return res.status(404).json({
+            status: 'Error',
+            message: 'Company does not exist'
+        });
+    }
+}
