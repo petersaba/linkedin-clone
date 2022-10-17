aecca91f08802093d026dfd2ce4991ab5eab85b9
Peter Saba <petersaba001@gmail.com>
2022-10-17 15:29:32 +0300

    frontend <> login: adding components to login route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import LoginNav from './components/login-nav';
+import LoginSection from './components/login-main-section';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={
-          <LoginNav/>
+          <>
+            <LoginNav/>
+            <LoginSection/>
+          </>
         }/>
       
       </Routes> 
