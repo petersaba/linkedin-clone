c3e5c88ee96d7f9890a3ad64db5923ad555781f5
Peter Saba <petersaba001@gmail.com>
2022-10-17 05:25:39 +0300

    backend <> routes: adding route for get job applicants

--- backend/routes/jobs.routes.js
+++ backend/routes/jobs.routes.js
@@ -1,5 +1,5 @@
 const Router = require('express');
-const { createJob, deleteJob, getAllJobs, getJobById } = require('../controllers/jobs.controller');
+const { createJob, deleteJob, getAllJobs, getJobById, getAllApplicants } = require('../controllers/jobs.controller');
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.post('/', createJob);
 router.delete('/', deleteJob);
 router.get('/', getAllJobs);
 router.get('/job/', getJobById);
+router.get('/applicants', getAllApplicants);
 
 module.exports = router;
