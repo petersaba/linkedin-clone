13e1c5898117d1f0be820eac211b93ddd0c8d2c4
Peter Saba <petersaba001@gmail.com>
2022-10-17 04:34:19 +0300

    backend <> utilities: adding function to send email

--- backend/utilities/utilities.js
+++ backend/utilities/utilities.js
@@ -1,4 +1,13 @@
 const fs = require('fs');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: process.env.SERVICE,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
 
 module.exports.base64ToImage = (base64_image, file_name) => {
     base64_str = base64_image.split(',')[1];
@@ -7,4 +16,21 @@ module.exports.base64ToImage = (base64_image, file_name) => {
     fs.writeFileSync(`./images/${file_name}.jpg`, buffer);
 
     return file_name;
+}
+
+module.exports.sendEmail = (receiver_email, subject, content) => {
+    const options = {
+        from: process.env.EMAIL,
+        to: receiver_email,
+        subject: subject,
+        text: content
+    }
+
+    transporter.sendMail(options, (err, info) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log("Sent " + info.response);
+    });
 }
