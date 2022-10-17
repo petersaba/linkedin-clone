8d3746b70f25b42391bca68dbf285c9abd2bf936
Peter Saba <petersaba001@gmail.com>
2022-10-17 08:28:02 +0300

    frontend <> login nav: fixing bugs

--- frontend/src/components/login-nav/index.js
+++ frontend/src/components/login-nav/index.js
@@ -1,9 +1,13 @@
-const LoginNav = ({}) => {
-    <nav className="login-nav">
-        <h2>Linked<span>in</span></h2>
-        <div>
-            <button>Join now</button>
-            <button>Sign in</button>
-        </div>
-    </nav>
+const LoginNav = () => {
+    return(
+        <nav className="login-nav">
+            <h2>Linked<span>in</span></h2>
+            <div>
+                <button>Join now</button>
+                <button>Sign in</button>
+            </div>
+        </nav>
+    );
 }
+
+export default LoginNav;
