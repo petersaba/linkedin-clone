231a2a9653003ae5f4c50969a1a8c0b8ecc38837
Peter Saba <petersaba001@gmail.com>
2022-10-17 11:04:22 +0300

    frontend <> login: adding html for login section

---
+++ frontend/src/components/login-main-section/index.js
@@ -0,0 +1,19 @@
+import './styles.css';
+import LoginInput from '../loginInput';
+
+const LoginSection = () => {
+    return (
+        <section className='main-section'>
+            <div>
+                <h1>Welcome to your professional community</h1>
+                <LoginInput type="text" id="email" name='Email'/>
+                <LoginInput type="password" id="password" name='Password'/>
+            </div>
+            <div>
+                <img src="images/login_image.svg" alt="image is not showing" />
+            </div>
+        </section>
+    );  
+}
+
+export default LoginSection;
