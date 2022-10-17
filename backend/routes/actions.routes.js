9cc38d61aee573c9801b843e5e32cf73fa7acc00
Peter Saba <petersaba001@gmail.com>
2022-10-17 03:34:03 +0300

    backend <> routes: adding route for applying for a job

--- backend/routes/actions.routes.js
+++ backend/routes/actions.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { followCompany, unfollowCompany } = require('../controllers/actions.controller')
+const { followCompany, unfollowCompany, applyForJob } = require('../controllers/actions.controller')
 
 const router = Router();
 
 router.post('/follow', followCompany);
 router.delete('/follow', unfollowCompany);
+router.post('/apply', applyForJob);
 
 module.exports = router;
