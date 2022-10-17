43ec4e13d30e0f9b3dd66037e0dce87d5b034194
Peter Saba <petersaba001@gmail.com>
2022-10-17 14:30:58 +0300

    frontend <> login: adding and styling sign in button

--- frontend/src/components/login-main-section/index.js
+++ frontend/src/components/login-main-section/index.js
@@ -8,6 +8,7 @@ const LoginSection = () => {
                 <h1>Welcome to your professional community</h1>
                 <LoginInput type="text" id="email" name='Email'/>
                 <LoginInput type="password" id="password" name='Password'/>
+                <button>Sign in</button>
             </div>
             <div>
                 <img src="images/login_image.svg" alt="image is not showing" />
