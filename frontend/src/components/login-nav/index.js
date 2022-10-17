a29bd49f9800d759c1053c4156939bc208241c45
Peter Saba <petersaba001@gmail.com>
2022-10-17 08:59:42 +0300

    frontend <> login nav: changing layout

--- frontend/src/components/login-nav/index.js
+++ frontend/src/components/login-nav/index.js
@@ -3,10 +3,12 @@ import './styles.css';
 const LoginNav = () => {
     return(
         <nav className="login-nav">
-            <h2>Linked<span>in</span></h2>
             <div>
-                <button>Join now</button>
-                <button>Sign in</button>
+                <h2>Linked<span>in</span></h2>
+                <div>
+                    <button>Join now</button>
+                    <button>Sign in</button>
+                </div>
             </div>
         </nav>
     );
