c193c82ae4c5dd0fa4c46bff397b6555a389e582
Peter Saba <petersaba001@gmail.com>
2022-10-17 08:18:38 +0300

    frontend <> login navbar: adding html for login nav bar

---
+++ frontend/src/components/login-nav/index.js
@@ -0,0 +1,9 @@
+const LoginNav = ({}) => {
+    <nav className="login-nav">
+        <h2>Linked<span>in</span></h2>
+        <div>
+            <button>Join now</button>
+            <button>Sign in</button>
+        </div>
+    </nav>
+}
