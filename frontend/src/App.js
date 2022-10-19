888ffcf0e3510d65756313dfffe5e7a2c8769e3f
Peter Saba <petersaba001@gmail.com>
2022-10-19 15:39:59 +0300

    frontend <> general: adding react-query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,27 @@ import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import LoginNav from './components/login-nav';
 import LoginSection from './components/login-main-section';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+export const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <LoginNav/>
-            <LoginSection/>
-          </>
-        }/>
-      
-      </Routes> 
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient} >
+      <ReactQueryDevtools initialIsOpen />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <LoginNav/>
+              <LoginSection/>
+            </>
+          }/>
+        
+        </Routes> 
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
